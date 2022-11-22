3d0df50777c954d85a11c029d65edbfce4bd3132
infstka <malder100@tut.by>
2022-11-22 10:42:50 +0300

    modified LR6

--- LR6/ClientNPt/ClientNPt.cpp
+++ LR6/ClientNPt/ClientNPt.cpp
@@ -12,53 +12,53 @@ string GetErrorPipe(int code)
 
 	switch (code)
 	{
-	case WSAEINTR:				 msgText = "Р Р°Р±РѕС‚Р° С„СѓРЅРєС†РёРё РїСЂРµСЂРІР°РЅР°\n";						  break;
-	case WSAEACCES:				 msgText = "Р Р°Р·СЂРµС€РµРЅРёРµ РѕС‚РІРµСЂРіРЅСѓС‚Рѕ\n";						  break;
-	case WSAEFAULT:				 msgText = "РћС€РёР±РѕС‡РЅС‹Р№ Р°РґСЂРµСЃ\n";								  break;
-	case WSAEINVAL:				 msgText = "РћС€РёР±РєР° РІ Р°СЂРіСѓРјРµРЅС‚Рµ\n";							  break;
-	case WSAEMFILE:				 msgText = "РЎР»РёС€РєРѕРј РјРЅРѕРіРѕ С„Р°Р№Р»РѕРІ РѕС‚РєСЂС‹С‚Рѕ\n";				  break;
-	case WSAEWOULDBLOCK:		 msgText = "Р РµСЃСѓСЂСЃ РІСЂРµРјРµРЅРЅРѕ РЅРµРґРѕСЃС‚СѓРїРµРЅ\n";					  break;
-	case WSAEINPROGRESS:		 msgText = "РћРїРµСЂР°С†РёСЏ РІ РїСЂРѕС†РµСЃСЃРµ СЂР°Р·РІРёС‚РёСЏ\n";				  break;
-	case WSAEALREADY: 			 msgText = "РћРїРµСЂР°С†РёСЏ СѓР¶Рµ РІС‹РїРѕР»РЅСЏРµС‚СЃСЏ\n";					  break;
-	case WSAENOTSOCK:   		 msgText = "РЎРѕРєРµС‚ Р·Р°РґР°РЅ РЅРµРїСЂР°РІРёР»СЊРЅРѕ\n";						  break;
-	case WSAEDESTADDRREQ:		 msgText = "РўСЂРµР±СѓРµС‚СЃСЏ Р°РґСЂРµСЃ СЂР°СЃРїРѕР»РѕР¶РµРЅРёСЏ\n";				  break;
-	case WSAEMSGSIZE:  			 msgText = "РЎРѕРѕР±С‰РµРЅРёРµ СЃР»РёС€РєРѕРј РґР»РёРЅРЅРѕРµ\n";				      break;
-	case WSAEPROTOTYPE:			 msgText = "РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ С‚РёРї РїСЂРѕС‚РѕРєРѕР»Р° РґР»СЏ СЃРѕРєРµС‚Р°\n";		  break;
-	case WSAENOPROTOOPT:		 msgText = "РћС€РёР±РєР° РІ РѕРїС†РёРё РїСЂРѕС‚РѕРєРѕР»Р°\n";					  break;
-	case WSAEPROTONOSUPPORT:	 msgText = "РџСЂРѕС‚РѕРєРѕР» РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ\n";					  break;
-	case WSAESOCKTNOSUPPORT:	 msgText = "РўРёРї СЃРѕРєРµС‚Р° РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ\n";				  break;
-	case WSAEOPNOTSUPP:			 msgText = "РћРїРµСЂР°С†РёСЏ РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ\n";					  break;
-	case WSAEPFNOSUPPORT:		 msgText = "РўРёРї РїСЂРѕС‚РѕРєРѕР»РѕРІ РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ\n";			  break;
-	case WSAEAFNOSUPPORT:		 msgText = "РўРёРї Р°РґСЂРµСЃРѕРІ РЅРµ РїРѕРґРґРµСЂР¶РёРІР°РµС‚СЃСЏ РїСЂРѕС‚РѕРєРѕР»РѕРј\n";	  break;
-	case WSAEADDRINUSE:			 msgText = "РђРґСЂРµСЃ СѓР¶Рµ РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ\n";						  break;
-	case WSAEADDRNOTAVAIL:		 msgText = "Р—Р°РїСЂРѕС€РµРЅРЅС‹Р№ Р°РґСЂРµСЃ РЅРµ РјРѕР¶РµС‚ Р±С‹С‚СЊ РёСЃРїРѕР»СЊР·РѕРІР°РЅ\n";	  break;
-	case WSAENETDOWN:			 msgText = "РЎРµС‚СЊ РѕС‚РєР»СЋС‡РµРЅР°\n";								  break;
-	case WSAENETUNREACH:		 msgText = "РЎРµС‚СЊ РЅРµ РґРѕСЃС‚РёР¶РёРјР°\n";							  break;
-	case WSAENETRESET:			 msgText = "РЎРµС‚СЊ СЂР°Р·РѕСЂРІР°Р»Р° СЃРѕРµРґРёРЅРµРЅРёРµ\n";					  break;
-	case WSAECONNABORTED:		 msgText = "РџСЂРѕРіСЂР°РјРјРЅС‹Р№ РѕС‚РєР°Р· СЃРІСЏР·Рё\n";						  break;
-	case WSAECONNRESET:			 msgText = "РЎРІСЏР·СЊ РІРѕСЃСЃС‚Р°РЅРѕРІР»РµРЅР°\n";							  break;
-	case WSAENOBUFS:			 msgText = "РќРµ С…РІР°С‚Р°РµС‚ РїР°РјСЏС‚Рё РґР»СЏ Р±СѓС„РµСЂРѕРІ\n";				  break;
-	case WSAEISCONN:			 msgText = "РЎРѕРєРµС‚ СѓР¶Рµ РїРѕРґРєР»СЋС‡РµРЅ\n";							  break;
-	case WSAENOTCONN:			 msgText = "РЎРѕРєРµС‚ РЅРµ РїРѕРґРєР»СЋС‡РµРЅ\n";							  break;
-	case WSAESHUTDOWN:			 msgText = "РќРµР»СЊР·СЏ РІС‹РїРѕР»РЅРёС‚СЊ send: СЃРѕРєРµС‚ Р·Р°РІРµСЂС€РёР» СЂР°Р±РѕС‚Сѓ\n";  break;
-	case WSAETIMEDOUT:			 msgText = "Р—Р°РєРѕРЅС‡РёР»СЃСЏ РѕС‚РІРµРґРµРЅРЅС‹Р№ РёРЅС‚РµСЂРІР°Р»  РІСЂРµРјРµРЅРё\n";		  break;
-	case WSAECONNREFUSED:		 msgText = "РЎРѕРµРґРёРЅРµРЅРёРµ РѕС‚РєР»РѕРЅРµРЅРѕ\n";						  break;
-	case WSAEHOSTDOWN:			 msgText = "РҐРѕСЃС‚ РІ РЅРµСЂР°Р±РѕС‚РѕСЃРїРѕСЃРѕР±РЅРѕРј СЃРѕСЃС‚РѕСЏРЅРёРё\n";			  break;
-	case WSAEHOSTUNREACH:		 msgText = "РќРµС‚ РјР°СЂС€СЂСѓС‚Р° РґР»СЏ С…РѕСЃС‚Р°\n";						  break;
-	case WSAEPROCLIM:			 msgText = "РЎР»РёС€РєРѕРј РјРЅРѕРіРѕ РїСЂРѕС†РµСЃСЃРѕРІ\n";						  break;
-	case WSASYSNOTREADY:		 msgText = "РЎРµС‚СЊ РЅРµ РґРѕСЃС‚СѓРїРЅР°\n";							  break;
-	case WSAVERNOTSUPPORTED:	 msgText = "Р”Р°РЅРЅР°СЏ РІРµСЂСЃРёСЏ РЅРµРґРѕСЃС‚СѓРїРЅР°\n";					  break;
-	case WSANOTINITIALISED:		 msgText = "РќРµ РІС‹РїРѕР»РЅРµРЅР° РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ WS2_32.DLL\n";		  break;
-	case WSAEDISCON:			 msgText = "Р’С‹РїРѕР»РЅСЏРµС‚СЃСЏ РѕС‚РєР»СЋС‡РµРЅРёРµ\n";						  break;
-	case WSATYPE_NOT_FOUND:		 msgText = "РљР»Р°СЃСЃ РЅРµ РЅР°Р№РґРµРЅ\n";								  break;
-	case WSAHOST_NOT_FOUND:		 msgText = "РҐРѕСЃС‚ РЅРµ РЅР°Р№РґРµРЅ\n";								  break;
-	case WSATRY_AGAIN:			 msgText = "РќРµР°РІС‚РѕСЂРёР·РёСЂРѕРІР°РЅРЅС‹Р№ С…РѕСЃС‚ РЅРµ РЅР°Р№РґРµРЅ\n";			  break;
-	case WSANO_RECOVERY:		 msgText = "РќРµРѕРїСЂРµРґРµР»РµРЅРЅР°СЏ РѕС€РёР±РєР°\n";						  break;
-	case WSANO_DATA:			 msgText = "РќРµС‚ Р·Р°РїРёСЃРё Р·Р°РїСЂРѕС€РµРЅРЅРѕРіРѕ С‚РёРїР°\n";				  break;
-	case WSAEINVALIDPROCTABLE:	 msgText = "РћС€РёР±РѕС‡РЅС‹Р№ СЃРµСЂРІРёСЃ\n";							  break;
-	case WSAEINVALIDPROVIDER:	 msgText = "РћС€РёР±РєР° РІ РІРµСЂСЃРёРё СЃРµСЂРІРёСЃР°\n";						  break;
-	case WSAEPROVIDERFAILEDINIT: msgText = "РќРµРІРѕР·РјРѕР¶РЅРѕ РёРЅРёС†РёР°Р»РёР·РёСЂРѕРІР°С‚СЊ СЃРµСЂРІРёСЃ\n";			  break;
-	case WSASYSCALLFAILURE:		 msgText = "РђРІР°СЂРёР№РЅРѕРµ Р·Р°РІРµСЂС€РµРЅРёРµ СЃРёСЃС‚РµРјРЅРѕРіРѕ РІС‹Р·РѕРІР°\n";		  break;
+	case WSAEINTR:				 msgText = "Работа функции прервана\n";						  break;
+	case WSAEACCES:				 msgText = "Разрешение отвергнуто\n";						  break;
+	case WSAEFAULT:				 msgText = "Ошибочный адрес\n";								  break;
+	case WSAEINVAL:				 msgText = "Ошибка в аргументе\n";							  break;
+	case WSAEMFILE:				 msgText = "Слишком много файлов открыто\n";				  break;
+	case WSAEWOULDBLOCK:		 msgText = "Ресурс временно недоступен\n";					  break;
+	case WSAEINPROGRESS:		 msgText = "Операция в процессе развития\n";				  break;
+	case WSAEALREADY: 			 msgText = "Операция уже выполняется\n";					  break;
+	case WSAENOTSOCK:   		 msgText = "Сокет задан неправильно\n";						  break;
+	case WSAEDESTADDRREQ:		 msgText = "Требуется адрес расположения\n";				  break;
+	case WSAEMSGSIZE:  			 msgText = "Сообщение слишком длинное\n";				      break;
+	case WSAEPROTOTYPE:			 msgText = "Неправильный тип протокола для сокета\n";		  break;
+	case WSAENOPROTOOPT:		 msgText = "Ошибка в опции протокола\n";					  break;
+	case WSAEPROTONOSUPPORT:	 msgText = "Протокол не поддерживается\n";					  break;
+	case WSAESOCKTNOSUPPORT:	 msgText = "Тип сокета не поддерживается\n";				  break;
+	case WSAEOPNOTSUPP:			 msgText = "Операция не поддерживается\n";					  break;
+	case WSAEPFNOSUPPORT:		 msgText = "Тип протоколов не поддерживается\n";			  break;
+	case WSAEAFNOSUPPORT:		 msgText = "Тип адресов не поддерживается протоколом\n";	  break;
+	case WSAEADDRINUSE:			 msgText = "Адрес уже используется\n";						  break;
+	case WSAEADDRNOTAVAIL:		 msgText = "Запрошенный адрес не может быть использован\n";	  break;
+	case WSAENETDOWN:			 msgText = "Сеть отключена\n";								  break;
+	case WSAENETUNREACH:		 msgText = "Сеть не достижима\n";							  break;
+	case WSAENETRESET:			 msgText = "Сеть разорвала соединение\n";					  break;
+	case WSAECONNABORTED:		 msgText = "Программный отказ связи\n";						  break;
+	case WSAECONNRESET:			 msgText = "Связь восстановлена\n";							  break;
+	case WSAENOBUFS:			 msgText = "Не хватает памяти для буферов\n";				  break;
+	case WSAEISCONN:			 msgText = "Сокет уже подключен\n";							  break;
+	case WSAENOTCONN:			 msgText = "Сокет не подключен\n";							  break;
+	case WSAESHUTDOWN:			 msgText = "Нельзя выполнить send: сокет завершил работу\n";  break;
+	case WSAETIMEDOUT:			 msgText = "Закончился отведенный интервал  времени\n";		  break;
+	case WSAECONNREFUSED:		 msgText = "Соединение отклонено\n";						  break;
+	case WSAEHOSTDOWN:			 msgText = "Хост в неработоспособном состоянии\n";			  break;
+	case WSAEHOSTUNREACH:		 msgText = "Нет маршрута для хоста\n";						  break;
+	case WSAEPROCLIM:			 msgText = "Слишком много процессов\n";						  break;
+	case WSASYSNOTREADY:		 msgText = "Сеть не доступна\n";							  break;
+	case WSAVERNOTSUPPORTED:	 msgText = "Данная версия недоступна\n";					  break;
+	case WSANOTINITIALISED:		 msgText = "Не выполнена инициализация WS2_32.DLL\n";		  break;
+	case WSAEDISCON:			 msgText = "Выполняется отключение\n";						  break;
+	case WSATYPE_NOT_FOUND:		 msgText = "Класс не найден\n";								  break;
+	case WSAHOST_NOT_FOUND:		 msgText = "Хост не найден\n";								  break;
+	case WSATRY_AGAIN:			 msgText = "Неавторизированный хост не найден\n";			  break;
+	case WSANO_RECOVERY:		 msgText = "Неопределенная ошибка\n";						  break;
+	case WSANO_DATA:			 msgText = "Нет записи запрошенного типа\n";				  break;
+	case WSAEINVALIDPROCTABLE:	 msgText = "Ошибочный сервис\n";							  break;
+	case WSAEINVALIDPROVIDER:	 msgText = "Ошибка в версии сервиса\n";						  break;
+	case WSAEPROVIDERFAILEDINIT: msgText = "Невозможно инициализировать сервис\n";			  break;
+	case WSASYSCALLFAILURE:		 msgText = "Аварийное завершение системного вызова\n";		  break;
 	default:					 msgText = "Have no connection with Named pipe\n";			  break;
 	};
 
@@ -72,10 +72,6 @@ string SetErrorPipe(string msgText, int code)
 
 int main()
 {
-
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
-
 	cout << "ClientNPt" << endl << endl;
 	int start_time = clock();
 	try
@@ -91,16 +87,16 @@ int main()
 
 		for (int i = 0; i < 10; i++)
 		{
-			//РѕС‚РєСЂС‹С‚СЊ РєР°РЅР°Р» (РїРѕРґРєР»СЋС‡ РєР»РёРµРЅС‚Р° Рє РёРјРµРЅРѕРІР°РЅРЅРѕРјСѓ РєР°РЅР°Р»Сѓ)
-			// //(СЃРёРјРІ РёРјСЏ РєР°РЅР°Р»Р° , С‡С‚РµРЅРёРµ/Р·Р°РїРёСЃСЊ РІ РєР°РЅР°Р», СЂРµР¶РёРј СЃРѕРІРј РёСЃРї, Р°С‚СЂРёР±СѓС‚С‹ Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё, С„Р»Р°Рі РѕС‚РєСЂС‹С‚РёСЏ РєР°РЅР°Р»Р°, С„Р»Р°РіРё Рё Р°С‚СЂРёР±СѓС‚С‹, РґРѕРї Р°С‚СЂРёР±СѓС‚С‹)
-			// . - Р»РѕРє РєРѕРјРї
+			//открыть канал (подключ клиента к именованному каналу)
+			// //(симв имя канала , чтение/запись в канал, режим совм исп, атрибуты безопасности, флаг открытия канала, флаги и атрибуты, доп атрибуты)
+			// . - лок комп
 			if ((hNP = CreateFile(L"\\\\.\\pipe\\Tube", GENERIC_WRITE | GENERIC_READ, FILE_SHARE_WRITE | FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL)) == INVALID_HANDLE_VALUE)
 			{
 				throw SetErrorPipe("CeateFile: ", GetLastError());
 			}
 
-			//РёР·РјРµРЅРµРЅРёРµ С…Р°СЂР°РєС‚РµСЂРёСЃС‚РёРє РєР°РЅР°Р»Р°
-			//(РґРµСЃРєСЂ РєР°РЅР°Р»Р°, СѓРєР°Р· РЅР° РЅРѕРІРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ, СѓРєР°Р· РЅР° РјР°РєСЃ РєРѕР»-РІРѕ Р±Р°Р№С‚РѕРІ, СѓРєР°Р· РЅР° РёРЅС‚ Р·Р°РґРµСЂР¶РєРё)
+			//изменение характеристик канала
+			//(дескр канала, указ на новое состояние, указ на макс кол-во байтов, указ на инт задержки)
 			if (!SetNamedPipeHandleState(hNP, &dwMode, NULL, NULL))
 			{
 				throw SetErrorPipe("SetNamedPipeHandleState: ", GetLastError());
@@ -113,8 +109,8 @@ int main()
 			strcat(buf, inum);
 			wb = sizeof(wbuf);
 
-			//(РґРµРєСЃСЂ РєР°РЅР°Р»Р°, СѓРєР°Р· РЅР° Р±СѓС„РµСЂ РґР»СЏ Р·Р°РїРёСЃРё, СЂР°Р·РјРµСЂ Р±СѓС„РµСЂР° РґР»СЏ Р·Р°РїРёСЃРё, СѓРєР°Р· РґР»СЏ С‡С‚РµРЅРёСЏ, СЂР°Р·РјРµСЂ РґР»СЏ С‡С‚РµРЅРёСЏ, РєРѕР»-РІРѕ РїСЂРѕС‡РёС‚ Р±Р°Р№С‚, Р±РµР· Р°СЃРёРЅС…СЂ РґРѕСЃС‚СѓРїР°)
-			//РѕР±СЉРµРґРёРЅСЏРµС‚ РѕРїРµСЂР°С†РёРё С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё РІ РѕРґРЅСѓ РѕРїРµСЂР°С†РёСЋ
+			//(декср канала, указ на буфер для записи, размер буфера для записи, указ для чтения, размер для чтения, кол-во прочит байт, без асинхр доступа)
+			//объединяет операции чтения и записи в одну операцию
 			if (!TransactNamedPipe(hNP, buf, wb, rbuf, rb, &rb, NULL))
 			{
 				throw SetErrorPipe("TransactNamedPipe: ", GetLastError());
@@ -130,8 +126,8 @@ int main()
 			throw SetErrorPipe("CreateFile: ", GetLastError());
 		}
 
-		//РёР·РјРµРЅРµРЅРёРµ С…Р°СЂР°РєС‚РµСЂРёСЃС‚РёРє РєР°РЅР°Р»Р°
-		//(РґРµСЃРєСЂ РєР°РЅР°Р»Р°, СѓРєР°Р· РЅР° РЅРѕРІРѕРµ СЃРѕСЃС‚РѕСЏРЅРёРµ, СѓРєР°Р· РЅР° РјР°РєСЃ РєРѕР»-РІРѕ Р±Р°Р№С‚РѕРІ, СѓРєР°Р· РЅР° РёРЅС‚ Р·Р°РґРµСЂР¶РєРё)
+		//изменение характеристик канала
+		//(дескр канала, указ на новое состояние, указ на макс кол-во байтов, указ на инт задержки)
 		if (!SetNamedPipeHandleState(hNP, &dwMode, NULL, NULL))
 		{
 			throw SetErrorPipe("SetNamedPipeHandleState: ", GetLastError());
@@ -140,8 +136,8 @@ int main()
 		char chr[] = " ";
 		DWORD lchr = sizeof(chr);
 
-		//(РґРµРєСЃСЂ РєР°РЅР°Р»Р°, СѓРєР°Р· РЅР° Р±СѓС„РµСЂ РґР»СЏ Р·Р°РїРёСЃРё, СЂР°Р·РјРµСЂ Р±СѓС„РµСЂР° РґР»СЏ Р·Р°РїРёСЃРё, СѓРєР°Р· РґР»СЏ С‡С‚РµРЅРёСЏ, СЂР°Р·РјРµСЂ РґР»СЏ С‡С‚РµРЅРёСЏ, РєРѕР»-РІРѕ РїСЂРѕС‡РёС‚ Р±Р°Р№С‚, Р±РµР· Р°СЃРёРЅС…СЂ РґРѕСЃС‚СѓРїР°)
-		//РѕР±СЉРµРґРёРЅСЏРµС‚ РѕРїРµСЂР°С†РёРё С‡С‚РµРЅРёСЏ Рё Р·Р°РїРёСЃРё РІ РѕРґРЅСѓ РѕРїРµСЂР°С†РёСЋ
+		//(декср канала, указ на буфер для записи, размер буфера для записи, указ для чтения, размер для чтения, кол-во прочит байт, без асинхр доступа)
+		//объединяет операции чтения и записи в одну операцию
 		if (!TransactNamedPipe(hNP, chr, lchr, rbuf, rb, &rb, NULL))
 		{
 			throw SetErrorPipe("TransactNamedPipe: ", GetLastError());
